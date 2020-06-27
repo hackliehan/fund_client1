ab20aaa5125f715b10309d5b5efc315fa052b2b3
binhuguang <binhuguang@foxmail.com>
2020-06-28 00:30:05 +0800

    no message

---
+++ src/config/path.js
@@ -0,0 +1,14 @@
+
+const basePath = 'http://localhost:4000';
+
+export const p_login = `${basePath}/api/login/account`;
+
+export const p_fund_backtetst = `${basePath}/fund/backtest`;
+
+export const p_fund_sty_search = `${basePath}/fund/searchSty`;
+
+export const p_fund_sty_add = `${basePath}/fund/addSty`;
+
+export const p_fund_sty_del = `${basePath}/fund/delSty'`;
+
+export const p_fund_sty_update = `${basePath}/fund/updateSty`;
